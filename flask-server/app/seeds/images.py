2048804bef0f1051ebee12f2deb6121d3f570dbc
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2022-03-11 11:17:53 -0800

    added requirements file and updated seed data link

--- flask-server/app/seeds/images.py
+++ flask-server/app/seeds/images.py
@@ -5,7 +5,7 @@ import urllib.request
 
 def seed_images():
     new_images = []
-    link = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/4fc14642-a3d6-424b-b621-5ecf5d955d7f/foam-seed.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220309%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220309T223656Z&X-Amz-Expires=86400&X-Amz-Signature=1ab450b219a76efba3016ca7d600bad0f5e59af425fe04f30c73e434333f369f&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22foam-seed.json%22&x-id=GetObject'
+    link = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/4fc14642-a3d6-424b-b621-5ecf5d955d7f/foam-seed.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220311%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220311T190529Z&X-Amz-Expires=86400&X-Amz-Signature=a0153129abfd8e7c2f9fe14b92d96f5dc0aca998aeee36471a924edb775ca8ac&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22foam-seed.json%22&x-id=GetObject'
     contents  = urllib.request.urlopen(link)
     data = json.load(contents)
     for obj in data:
